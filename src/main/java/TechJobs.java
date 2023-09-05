114cd562381c5d0cbbe62d1dac3861e9fcced766
EzRex <ezekielle.duller@gmail.com>
2023-09-04 21:41:00 -0400

    Did first part of printJobs, printing all jobs. Fixed warnings

--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -120,7 +120,7 @@ public class TechJobs {
     // Print a list of jobs
     //https://www.geeksforgeeks.org/traverse-through-a-hashmap-in-java/ reference for forEach on HashMap
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
-        for (HashMap job: someJobs) {
+        for (HashMap<String,String> job: someJobs) {
             System.out.println("****");
             job.forEach((k,v) -> System.out.println(k + ": " + v ));
             System.out.println("****\n");
