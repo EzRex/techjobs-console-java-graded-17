2bb7191d9857b9b260b069ebaf05e47261949bdb
EzRex <ezekielle.duller@gmail.com>
2023-09-04 21:59:49 -0400

    Completed Task 1

--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -120,12 +120,14 @@ public class TechJobs {
     // Print a list of jobs
     //https://www.geeksforgeeks.org/traverse-through-a-hashmap-in-java/ reference for forEach on HashMap
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
-        for (HashMap<String,String> job: someJobs) {
-            System.out.println("****");
-            job.forEach((k,v) -> System.out.println(k + ": " + v ));
-            System.out.println("****\n");
+        if (someJobs.size() == 0 || someJobs == null) {
+            System.out.print("No Results");
+        } else {
+            for (HashMap<String, String> job : someJobs) {
+                System.out.println("****");
+                job.forEach((k, v) -> System.out.println(k + ": " + v));
+                System.out.println("****\n");
+            }
         }
-
-        System.out.println("printJobs is not implemented yet");
     }
 }
